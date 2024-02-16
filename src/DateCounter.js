18342a21a6312373d86c6f3b57bae16724320280
WiseVizo <mecoc1011@gmail.com>
2024-02-16 13:26:53 +0530

    using useReducer hook

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,25 +1,38 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+function reducer(data, action) {
+  console.log(data, action);
+  if (action.type === "incriment") return data + 1;
+  if (action.type === "decriment") return data - 1;
+  if (action.type === "keyboardInput")
+    return action.payload ? action.payload : data;
+  if (action.type === "reset") return 0;
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0);
   const [step, setStep] = useState(1);
 
   // This mutates the date object.
-  const date = new Date("june 21 2027");
+  const date = new Date(); //"june 21 2027"
   date.setDate(date.getDate() + count);
 
   const dec = function () {
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    // setCount((count) => count - step);
+    dispatch({ type: "decriment", payload: -1 });
   };
 
   const inc = function () {
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    // setCount((count) => count + step);
+    dispatch({ type: "incriment" });
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    // setCount(Number(e.target.value));
+    dispatch({ type: "keyboardInput", payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
@@ -27,7 +40,8 @@ function DateCounter() {
   };
 
   const reset = function () {
-    setCount(0);
+    // setCount(0);
+    dispatch({ type: "reset" });
     setStep(1);
   };
 
