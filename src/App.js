27a410912482bff1d064307502bd467cfe7df54d
WiseVizo <mecoc1011@gmail.com>
2024-02-18 10:45:39 +0530

    fetching data from our fake api
    using a reducer hook for storing the qs data and different  status it can have like Ready , Error , Loading, ...

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
 // import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  status: "Loading", // possible status: Loading, Ready, Error, Active, Finished
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.payload, status: "Ready" };
+    case "dataFailed":
+      return { ...state, status: "Error" };
+    default:
+      throw new Error("Unknown Action");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    async function getQsData() {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+
+        if (res.ok === true) {
+          // console.log(res);
+          const data = await res.json();
+          dispatch({ type: "dataRecieved", payload: data });
+          // console.log(data);
+        }
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+        console.error(err);
+      }
+    }
+    getQsData();
+  }, []);
   return (
     <div className="app">
       {/* <DateCounter /> */}
       <Header />
       <Main>
         <p>1/15</p>
-        <p>Question?</p>
+        <p>qs</p>
       </Main>
     </div>
   );
